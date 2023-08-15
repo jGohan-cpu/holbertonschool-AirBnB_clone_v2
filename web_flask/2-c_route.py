ea9cf0b1b36fb3b18df0c69fc8daa599c977d8c2
Yaniel Ramos <yanielramos0504@gmail.com>
2023-08-15 19:45:21 -0400

    adding host and port

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,3 +23,7 @@ def c_is_fun(text):
     """replace _ for spaces"""
     text = text.replace('_', ' ')
     return f" C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
