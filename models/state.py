f377d144e2784c576e7bff4b83bd52898ef91aaa
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-12 12:59:47 -0400

    states changed

--- models/state.py
+++ models/state.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
 from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """State class"""
-    __tablename__ = 'states'
-    if getenv('HBNB_TYPE_Storage') == 'db':
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-
+        cities = relationship("City", backref="state")
     else:
-        name = ''
+        name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """Getter"""
+            """getter for list of city instances related to the state"""
             city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
