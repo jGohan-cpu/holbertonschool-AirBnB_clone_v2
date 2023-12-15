d26f85628f7a3a4ee62a5b6f6fb1722a38c52401
jGohan-cpu <jjoseriveram01@gmail.com>
2023-12-15 12:34:08 -0400

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,19 @@
-#!/usr/bin/python3
-"""Script that have 3 routes"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello():
-    """return Hello HBNB"""
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """return HBNB"""
+def hbnb():
     return "HBNB"
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """replace _ for spaces"""
+def c_text(text):
     text = text.replace('_', ' ')
-    return f" C {text}"
+    return "C " + text
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
